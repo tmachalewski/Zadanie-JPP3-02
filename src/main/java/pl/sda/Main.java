6435aadaced5913638c3dc9eca81436a405e3e71
tommach <tomek.machalewski@gmail.com>
2020-02-23 13:21:31 +0100

    Added differenc calculation

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,7 +1,25 @@
 package pl.sda;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What year?");
+        int year = sc.nextInt();
+        System.out.println("What month?");
+        int month = sc.nextInt();
+        System.out.println("What day?");
+        int day = sc.nextInt();
+
+        LocalDate now = LocalDate.now();
+        LocalDate entered = LocalDate.of(year, month, day);
+
+        System.out.println("Years:" + ChronoUnit.YEARS.between(entered, now));
+        System.out.println("Months:" + ChronoUnit.MONTHS.between(entered, now));
+        System.out.println("Days:" + ChronoUnit.DAYS.between(entered, now));
 
     }
 }
